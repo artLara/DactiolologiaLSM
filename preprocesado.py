2b17563aa3d85137ddea751061f34d9c94b9d922
artLara <art.lara.caz@gmail.com>
2022-11-08 22:56:29 -0600

    Prepreocesado changed

--- preprocesado.py
+++ preprocesado.py
@@ -2,38 +2,51 @@ import cv2
 import os
 import pandas as pd
 persona = 4
-dir_path = 'D:\\CIC\\LSM\\DactiolologiaLSM\\videos\\'
+dir_path = 'D:\\CIC\\LSM\\DactiolologiaLSM\\videosPersonas\\limpios\\'
 image_path = 'D:\\CIC\\LSM\\DactiolologiaLSM\\images\\'
 df = pd.DataFrame()
 imageNames = []
 targets = []
 # df['ImageName'] = namesTrain
 # df['Target'] = classesTrain
+personNum = 1
+for persona in os.listdir(dir_path):
+	print(persona)
+	videoPath = dir_path+persona+'\\'
+	for videoName in os.listdir(videoPath):
+		letter = videoName.split('_')[0]
+		hand = (videoName.split('_')[1]).split('.')[0]
+		# if letter != 'a':
+		# 	continue
+		print(videoName, letter, hand)
+		capture = cv2.VideoCapture(videoPath+videoName)
+		frameNr = 0
+		while (True):
+			success, frame = capture.read()
+			if success:
+				imageName = letter + str(frameNr)+'_'+hand+'_'+str(personNum)+'.jpg'
+				imageNames.append(imageName)
+				imageName = image_path + imageName
 
-for videoName in os.listdir(dir_path):
-	letter = videoName.split('_')[0]
-	hand = (videoName.split('_')[1]).split('.')[0]
-	# if letter != 'a':
-	# 	continue
-	print(videoName, letter, hand)
-	capture = cv2.VideoCapture(dir_path+videoName)
-	frameNr = 0
-	while (True):
-		success, frame = capture.read()
-		if success:
-			imageName = letter + str(frameNr)+'_'+hand+'.jpg'
-			imageNames.append(imageName)
-			imageName = image_path + imageName
+				targets.append(letter)
+				if hand == "izq":
+					if persona != 'persona1':
+						cv2.imwrite(imageName, cv2.flip(frame, 1))
+					else:
+						cv2.imwrite(imageName, frame)
 
-			targets.append(letter)
-			if hand == "izq":
-				cv2.imwrite(imageName, cv2.flip(frame, 1))
+				else:
+					if persona == 'persona1':
+						cv2.imwrite(imageName, cv2.flip(frame, 1))
+					else:
+						cv2.imwrite(imageName, frame)
 			else:
-				cv2.imwrite(imageName, frame)
-		else:
-			break
-		frameNr += 1
-	capture.release()
+				break
+			frameNr += 1
+		capture.release()
+		# break
+	personNum +=1
+
 
 df['ImageName'] = imageNames
 df['Target'] = targets
